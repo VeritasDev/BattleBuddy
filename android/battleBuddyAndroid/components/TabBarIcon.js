ac4283faa41f47e8d6421ca721a3f990251daa27
Jordy Koppen <jordykoppen@users.noreply.github.com>
2019-09-18 22:12:08 +0200

    refactor css to styled-components for TabBarIcon

--- android/battleBuddyAndroid/components/TabBarIcon.js
+++ android/battleBuddyAndroid/components/TabBarIcon.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import {Image, StyleSheet} from 'react-native';
 import PropTypes from 'prop-types';
+import styled from 'styled-components/native';
 
-import Colors from '../constants/Colors';
+// For react-native only attributes like tintColor (non css props) use attrs as
+// styled components doesn't support that unfortunately.
+const Image = styled.Image.attrs(({focused}) => {
+  return {
+    tintColor: focused ? '#FF491C' : '#ccc'
+  };
+})`
+  margin-bottom: 3px;
+  transform: scale(0.5);
+`;
 
-export default function TabBarIcon(props) {
-  var icon;
-  switch (props.name) {
+export default function TabBarIcon({name, ...props}) {
+  let icon;
+  switch (name) {
     case 'items':
       icon = require('../assets/images/items.png');
       break;
@@ -20,28 +29,8 @@ export default function TabBarIcon(props) {
       return;
   }
 
-  return (
-    <Image
-      style={[styles.iconBase, props.focused && styles.selectedIcon]}
-      source={icon}
-    />
-  );
+  return <Image {...props} source={icon} />;
 }
-
-const styles = StyleSheet.create({
-  iconBase: {
-    marginBottom: -3,
-    transform: [
-      {
-        scale: 0.5
-      }
-    ],
-    tintColor: Colors.tabIconDefault
-  },
-  selectedIcon: {
-    tintColor: Colors.tabIconSelected
-  }
-});
 
 TabBarIcon.propTypes = {
   name: PropTypes.string.isRequired,
