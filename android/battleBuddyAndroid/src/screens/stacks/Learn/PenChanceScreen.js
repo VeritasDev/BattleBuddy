b24fe280542348775bc322b73ba0fe72740e85aa
mbanks94 <mbanks123456@gmail.com>
2019-10-21 21:51:39 -0500

    pen chance ui (placeholders)

--- android/battleBuddyAndroid/src/screens/stacks/Learn/PenChanceScreen.js
+++ android/battleBuddyAndroid/src/screens/stacks/Learn/PenChanceScreen.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import styled from 'styled-components/native';
+import Button from '../../../components/learn/Button';
 
-const ScrollView = styled.ScrollView`
+const View = styled.View`
   background: ${({theme}) => theme.colors.background};
+  height: 100%;
+  justify-content: center;
+  flex: 1;
+`;
+
+const HorizontalView = styled.View`
+  flex-direction: row;
+  justify-content: center;
+  flex: 1;
+`;
+
+const PenChance = styled.Text`
+  color: ${({theme}) => theme.colors.white};
+  font-size: 80;
+  flex: 1;
+  align-self: center;
+  text-align: center;
 `;
 
 const PenChanceScreen = () => {
-  return <ScrollView></ScrollView>;
+  return (
+    <View>
+      <HorizontalView>
+        <PenChance>_</PenChance>
+      </HorizontalView>
+      <HorizontalView>
+        <Button title="Select Ammo" />
+        <Button title="Select Armor" />
+      </HorizontalView>
+    </View>
+  );
 };
 
 PenChanceScreen.navigationOptions = {
