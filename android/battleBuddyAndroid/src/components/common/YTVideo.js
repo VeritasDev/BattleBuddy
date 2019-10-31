4c0b3daec55def608c5698053056770d2dd711c0
Jordy <24296556+jordykoppen@users.noreply.github.com>
2019-10-31 14:26:46 +0100

    Finished ballistics page

--- android/battleBuddyAndroid/src/components/common/YTVideo.js
+++ android/battleBuddyAndroid/src/components/common/YTVideo.js
@@ -1,33 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
-import {YouTubeStandaloneAndroid} from 'react-native-youtube';
+import YouTube from 'react-native-youtube';
 import styled from 'styled-components/native';
+import {Viewport} from '@skele/components';
 
-const Thumbnail = styled.TouchableOpacity`
-  background: red;
+/**
+ * Because of Android's limited Youtube API we can only have
+ * on Youtube player active at the same time. To work around this,
+ * I mount/unmount when component is about to hit the viewport.
+ *
+ * Check for more info: https://github.com/inProgress-team/react-native-youtube#multiple-youtube--instances-on-android
+ * Check Viewport Tacker: https://netcetera.gitbooks.io/skele/packages/components/
+ */
+
+const View = styled.View`
   /* prettier-ignore */
   aspectRatio: 1.77;
 `;
 
+const ViewportAwareView = Viewport.Aware(View);
+
 const YTVideo = ({videoId}) => {
-  const onPressHandler = () => {
-    YouTubeStandaloneAndroid.playVideo({
-      apiKey: 'AIzaSyBKtolM-loj-u-AIEh6HCH0BaL8UminqVI',
-      lightboxMode: true,
-      autoplay: true,
-      videoId
-    });
-  };
+  const [show, setShow] = useState(false);
 
-  return <Thumbnail onPress={onPressHandler}></Thumbnail>;
+  return (
+    <ViewportAwareView
+      preTriggerRatio={0.5}
+      onViewportEnter={() => setShow(true)}
+      onViewportLeave={() => setShow(false)}
+    >
+      {show && (
+        <YouTube
+          apiKey="AIzaSyBKtolM-loj-u-AIEh6HCH0BaL8UminqVI"
+          videoId={videoId}
+          style={{alignSelf: 'stretch', aspectRatio: 1.77}}
+        />
+      )}
+    </ViewportAwareView>
+  );
 };
-
-/* <YouTube
-      loop
-      apiKey="AIzaSyBKtolM-loj-u-AIEh6HCH0BaL8UminqVI"
-      videoId={videoId}
-      style={{alignSelf: 'stretch', aspectRatio: 1.77}}
-    /> */
 
 YTVideo.propTypes = {
   videoId: PropTypes.string.isRequired
--- android/battleBuddyAndroid/src/screens/stacks/Learn/BallisticsScreen.js
+++ android/battleBuddyAndroid/src/screens/stacks/Learn/BallisticsScreen.js
@@ -2,9 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components/native';
 import ScrollableContainer from '../../../components/common/ScrollableContainer';
-import Image from '../../../components/learn/Image';
+import BallisticsImage from '../../../components/learn/BallisticsImage';
 import localeString from '../../../utils/localeString';
 import YTVideo from '../../../components/common/YTVideo';
+import {Viewport} from '@skele/components';
 
 const Text = styled.Text`
   color: ${({theme}) => theme.colors.white};
@@ -37,44 +38,55 @@ const ScrollView = styled(ScrollableContainer)`
 
 const BallisticsScreen = () => {
   return (
-    <ScrollView>
-      <MainTitle>{localeString('ballistics_title')}</MainTitle>
-      <SubTitle>Veritas - 13/07/2019</SubTitle>
-      <Body>{localeString('ballistics_body_1')}</Body>
-      <Body>{localeString('ballistics_body_2')}</Body>
-      <Image name="1" />
+    // Check YTVideo.js for information why we use Viewport.Tracker here.
+    <Viewport.Tracker>
+      <ScrollView>
+        <BallisticsImage
+          source={require('../../../../assets/images/card_heroes/ballistics.png')}
+        />
+        <MainTitle>{localeString('ballistics_title')}</MainTitle>
+        <SubTitle>Veritas - 13/07/2019</SubTitle>
+        <Body>{localeString('ballistics_body_1')}</Body>
+        <Body>{localeString('ballistics_body_2')}</Body>
 
-      {/* ARMOR CLASS */}
-      <Bold>{localeString('ballistics_body_2_1_title')}</Bold>
-      <Body>{localeString('ballistics_body_2_1')}</Body>
+        <BallisticsImage
+          source={require('../../../../assets/images/card_heroes/armor.jpg')}
+        />
 
-      {/* DURABILITY */}
-      <Bold>{localeString('ballistics_body_2_2_title')}</Bold>
-      <Body>{localeString('ballistics_body_2_2_1')}</Body>
-      <YTVideo videoId="3KbFMHp4NOE" />
-      <Body>{localeString('ballistics_body_2_2_2')}</Body>
-      <Body>{localeString('ballistics_body_2_2_3')}</Body>
+        {/* ARMOR CLASS */}
+        <Bold>{localeString('ballistics_body_2_1_title')}</Bold>
+        <Body>{localeString('ballistics_body_2_1')}</Body>
 
-      <Image name="3" />
+        {/* DURABILITY */}
+        <Bold>{localeString('ballistics_body_2_2_title')}</Bold>
+        <Body>{localeString('ballistics_body_2_2_1')}</Body>
+        <YTVideo videoId="3KbFMHp4NOE" />
+        <Body>{localeString('ballistics_body_2_2_2')}</Body>
+        <Body>{localeString('ballistics_body_2_2_3')}</Body>
 
-      {/* ZONES OF PROTECTION */}
-      <Bold>{localeString('ballistics_body_2_3_title')}</Bold>
-      <Text>{localeString('ballistics_body_2_3')}</Text>
+        <BallisticsImage
+          source={require('../../../../assets/images/card_heroes/gen4.png')}
+        />
 
-      {/* MATERIALS */}
-      <Bold>{localeString('ballistics_body_2_4_title')}</Bold>
-      <Text>{localeString('ballistics_body_2_4')}</Text>
+        {/* ZONES OF PROTECTION */}
+        <Bold>{localeString('ballistics_body_2_3_title')}</Bold>
+        <Text>{localeString('ballistics_body_2_3')}</Text>
 
-      {/* PENALTIES */}
-      <Bold>{localeString('ballistics_body_2_5_title')}</Bold>
-      <Text>{localeString('ballistics_body_2_5')}</Text>
+        {/* MATERIALS */}
+        <Bold>{localeString('ballistics_body_2_4_title')}</Bold>
+        <Text>{localeString('ballistics_body_2_4')}</Text>
 
-      <YTVideo videoId="XDK-aLkGvkA" />
+        {/* PENALTIES */}
+        <Bold>{localeString('ballistics_body_2_5_title')}</Bold>
+        <Text>{localeString('ballistics_body_2_5')}</Text>
 
-      {/* SUMMARY */}
-      <Bold>{localeString('ballistics_body_3_title')}</Bold>
-      <Text>{localeString('ballistics_body_3')}</Text>
-    </ScrollView>
+        <YTVideo videoId="XDK-aLkGvkA" />
+
+        {/* SUMMARY */}
+        <Bold>{localeString('ballistics_body_3_title')}</Bold>
+        <Text>{localeString('ballistics_body_3')}</Text>
+      </ScrollView>
+    </Viewport.Tracker>
   );
 };
 
