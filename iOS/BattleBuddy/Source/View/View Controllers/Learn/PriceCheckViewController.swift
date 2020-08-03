0b421e9681025d5c8e53e4b4bdfa637463c5c1e8
Veritas <info@veritas.wtf>
2020-08-03 15:39:22 -0400

    Removed test code.

--- iOS/BattleBuddy/Source/View/View Controllers/Learn/PriceCheckViewController.swift
+++ iOS/BattleBuddy/Source/View/View Controllers/Learn/PriceCheckViewController.swift
@@ -82,22 +82,6 @@ extension PriceCheckViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         controller.updateSearchText(searchText)
     }
-
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.resignFirstResponder()
-    }
-}
-
-extension PriceCheckViewController {
-//    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
-//        searchBar.resignFirstResponder()
-//        print("AAAA")
-//    }
-
-    override func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        print("BBBB")
-        searchBar.resignFirstResponder()
-    }
 }
 
 extension PriceCheckViewController: PriceCheckControllerDelegate {
